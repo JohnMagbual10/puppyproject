96df56def0cba3f1fc7e91545dc5966bbdda5c30
John Magbual <jmagbual@Johns-Air.lan>
2024-04-01 22:27:30 -0700

    Use the Data in React

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import { useState } from 'react';
 import { puppyList } from './data.js';
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
+  const [puppies, setPuppies] = useState(puppyList);
+  console.log(puppies);
+  
+
   return (
     <>
       <div>
