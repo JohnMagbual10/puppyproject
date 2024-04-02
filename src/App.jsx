c58cf6974b73d70786883244a3bfa1bacbe16f07
John Magbual <jmagbual@Johns-Air.lan>
2024-04-01 22:43:09 -0700

    add more funrtionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
 import { useState } from 'react';
 import { puppyList } from './data.js';
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 
 function App() {
+  const [featPupId, setFeatPupId] = useState(null);
   const [puppies, setPuppies] = useState(puppyList);
   console.log(puppies);
-  
 
   return (
     <>
-      <div> { puppies.map((puppy) => {
-    return <p key={puppy.id}>{puppy.name}</p>;
-  })
-  }
+      <div>
+        {puppies.map((puppy) => (
+          <p onClick={() => setFeatPupId(puppy.id)} key={puppy.id}>{puppy.name}</p>
+        ))}
+        {featPupId && <p>{featPupId}</p>}
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -27,7 +28,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
